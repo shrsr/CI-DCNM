cf8245b09ed58c0240bdd3faa5be0cff4a1d6c06
srish <ssrish@cisco.com>
2019-07-21 21:59:36 -0700

    To Jenkins

--- testNetwork.py
+++ testNetwork.py
@@ -5,7 +5,7 @@ import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 class NetworkTestCase(unittest.TestCase):
-# Sample run 2
+# Sample run
     def testNetwork(self):
         requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
         fabric = FabricList()
